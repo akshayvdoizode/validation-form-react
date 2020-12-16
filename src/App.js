6142590a5866f865e043672631a351c837d98c67
akshay v doyizode <akshayvdoizode123@gmail.com>
2020-12-16 22:10:47 +0530

    final build

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+import "./App.css";
+import Form from "./Form";
+import Table from "./Table";
+
+injectTapEventPlugin();
+
+class App extends Component {
+  state = {
+    data: [],
+    editIdx: -1,
+  };
+
+  handleRemove = (i) => {
+    this.setState((state) => ({
+      data: state.data.filter((row, j) => j !== i),
+    }));
+  };
+
+  startEditing = (i) => {
+    this.setState({ editIdx: i });
+  };
+
+  stopEditing = () => {
+    this.setState({ editIdx: -1 });
+  };
+
+  handleChange = (e, name, i) => {
+    const { value } = e.target;
+    this.setState((state) => ({
+      data: state.data.map((row, j) =>
+        j === i ? { ...row, [name]: value } : row
+      ),
+    }));
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className="App">
+          <Form
+            onSubmit={(submission) =>
+              this.setState({
+                data: [...this.state.data, submission],
+              })
+            }
+          />
+          <Table
+            handleRemove={this.handleRemove}
+            startEditing={this.startEditing}
+            editIdx={this.state.editIdx}
+            stopEditing={this.stopEditing}
+            handleChange={this.handleChange}
+            data={this.state.data}
+            header={[
+              {
+                name: "First name",
+                prop: "firstName",
+              },
+              {
+                name: "Last name",
+                prop: "lastName",
+              },
+              {
+                name: "Username",
+                prop: "username",
+              },
+              {
+                name: "Email",
+                prop: "email",
+              },
+            ]}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
